a03b25f2a5a65aad0432b283e2e0d5582c766476
Guy Radcliffe <gr7432@gmail.com>
2013-02-06 12:00:43 -0500

    first commit

---
+++ onefile-include.php
@@ -0,0 +1,43 @@
+<?php
+$xmlstr = <<<XML
+<?xml version='1.0' standalone='yes'?>
+<site>
+  <sections>
+    <echo>
+      <about>
+        <headline>About ECHO</headline>
+        <content>Welcome to the ECFMG Certificate Holders Office (ECHO). ECHO is committed to providing support and service to ECFMG-certified physicians as they plan their careers. If you are certified by ECFMG, or are about to be certified, you already know that ECFMG Certification is just one step in your career development.</content>
+        <content>To pursue a career as a physician in the United States, you need to identify the medical specialty or specialties that are a good fit for your skill set, your personality, and your career objectives. You need to apply to the U.S. graduate medical education (GME) programs that are right for you.</content>
+      </about>
+      <acgme>
+        <headline>ACGME Core Competencies</headline>
+        <content>The Accreditation Council for Graduate Medical Education (ACGME) is a private, non-profit council that evaluates and accredits more than 9,000 residency programs across 135 specialties and subspecialties in the United States. The ACGME establishes educational standards and common program requirements for all training programs, and ACGME accreditation signifies a program's commitment to maintaining quality education for residents and safe care for patients. A program must be ACGME-accredited in order to receive federal graduate medical education (GME) funds.</content>
+        <content>In 2002, the ACGME launched a competency initiative called the Outcome Project. As a result of this project, ACGME identified six ACGME Core Competencies to be used by GME programs to evaluate their residents in training. The six ACGME Core Competencies are: patient care; medical knowledge; practice-based learning and improvement; interpersonal and communication skills; professionalism; and systems-based practice. Each competency is made up of different milestones residents are required to master at key stages of their medical training.</content>
+      </acgme>
+    </echo>
+    <eics>
+      <about>
+        <headline>About EICS</headline>
+        <content>For these clients, EICS obtains primary-source verification of the credentials of physician applicants who completed their medical education outside the authorities' jurisdiction. The physician credentials verified by EICS include medical school diplomas, medical school transcripts, certificates of postgraduate medical training, and certificates of medical registration or licensure. The ECFMG International Credentials Services (EICS) serves international organizations and authorities involved in medical registration, licensing, and assessment.</content>
+        <content>In providing this service, ECFMG utilizes its well-established and internationally recognized procedures for verifying medical education credentials, first developed for the purpose of ECFMG Certification.</content>
+      </about>
+      <credentials>
+        <headline>Verification of Credentials</headline>
+        <content>Primary-source verification is a rigorous, multi-step process that ECFMG employs to ensure the integrity and authenticity of a physician's medical education credentials.</content>
+        <content>When ECFMG receives a credential issued by a medical school, it first reviews the credential and compares it to sample primary-source verified documents from its Medical Credentials Reference Library to confirm that it is consistent with the format of credentials issued by the medical school. If the credential is determined to be consistent with verified samples, ECFMG will communicate directly with the issuing institution to request verification of the credential's authenticity. If ECFMG believes it will facilitate the verification process, ECFMG will communicate with an institution in the language of the country in which the institution is located. ECFMG currently verifies documents using 11 languages.</content>
+      </credentials>
+      <physicians>
+        <headline>EICS For Physicians</headline>
+        <content>EICS credentials verification is an application-based process offered to medical registration authorities. If your medical registration authority requires EICS verification of medical credentials, the authority will provide you with its customized EICS application materials or direct you to print the application materials from this website. If you are unsure whether your medical registration authority requires EICS verification of your credentials, please contact the medical registration authority.</content>
+        <content>EICS processes new applications within two weeks of receipt. EICS will notify you by e-mail once your application has been processed. This e-mail will include your EICS identification number and will instruct you if additional information or credentials are required. Please include your EICS identification number on all correspondence to EICS.</content>
+      </physicians>
+      <reports>
+        <headline>Credentials Verification Reports</headline>
+        <content>EICS Credentials Verification Reports provide detailed information on a physician's medical credentials and the verification status of his/her credentials to the medical registration authority. EICS reports are printed on watermarked security paper and sent via courier service to the medical registration authority.</content>
+        <content>The EICS Credentials Verification Report includes information on all medical credentials and verifications received and processed by EICS and all outstanding verifications. Copies of the physician's medical credentials and verifications are included with the report.</content>
+      </reports>
+    </eics>
+  </sections>
+</site>
+XML;
+?>
